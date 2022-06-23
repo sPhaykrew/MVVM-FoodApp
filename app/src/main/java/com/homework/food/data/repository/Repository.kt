d0d55cfc1ec82f9c082d38da3603a2c7e58a95b9
Sakda Phaykrew <s_phaykrew@hotmail.com>
2022-06-24 01:13:14 +0700

    update ui

--- app/src/main/java/com/homework/food/data/repository/Repository.kt
+++ app/src/main/java/com/homework/food/data/repository/Repository.kt
@@ -3,9 +3,6 @@ package com.homework.food.data.repository
 import com.homework.food.data.api.FoodApi
 import com.homework.food.data.local.FoodDAO
 import com.homework.food.data.model.FoodItem
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class Repository(private val foodApi : FoodApi , private val foodDAO: FoodDAO) {
 
